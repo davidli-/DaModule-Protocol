4c8717d2f9e08aa66da25f62eb7d9df89ea7fb11
davidli <1182622460@qq.com>
2019-09-03 00:09:43 +0800

    Initial Commit

---
+++ DaModule+Protocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DaModule+Protocol
+//
+//  Created by Macmafia on 2019/9/3.
+//  Copyright © 2019 Macmafia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DaModule+Protocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaModule+Protocol
+//
+//  Created by Macmafia on 2019/9/3.
+//  Copyright © 2019 Macmafia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
